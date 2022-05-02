9d8a994869e53d96466e9a7c73b456f3c6b2b638
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-05-03 01:24:40 +0600

    web middleware added for form validation

--- routes/web.php
+++ routes/web.php
@@ -27,68 +27,71 @@ Route::get('/', function () {
 /**
  * Authentication Route
  */
-Route::prefix(config('laraflow.auth.prefix'))->name('backend.auth.')->group(function () {
+Route::prefix(config('laraflow.auth.prefix'))
+    ->name('backend.auth.')
+    ->middleware('web')
+    ->group(function () {
 
-    Route::view('/privacy-terms', 'auth::terms')
-        ->name('terms');
+        Route::view('/privacy-terms', 'auth::terms')
+            ->name('terms');
 
-    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
-        ->middleware('guest')
-        ->name('login');
-
-    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-        ->middleware('guest');
-
-    if (config('laraflow.auth.allow_register')):
-        Route::get('/register', [RegisteredUserController::class, 'create'])
+        Route::get('/login', [AuthenticatedSessionController::class, 'create'])
             ->middleware('guest')
-            ->name('register');
+            ->name('login');
 
-        Route::post('/register', [RegisteredUserController::class, 'store'])
+        Route::post('/login', [AuthenticatedSessionController::class, 'store'])
             ->middleware('guest');
-    endif;
 
-    if (config('laraflow.auth.allow_password_reset')):
-        Route::get('/forgot-password', [PasswordResetController::class, 'create'])
+        if (config('laraflow.auth.allow_register')):
+            Route::get('/register', [RegisteredUserController::class, 'create'])
+                ->middleware('guest')
+                ->name('register');
+
+            Route::post('/register', [RegisteredUserController::class, 'store'])
+                ->middleware('guest');
+        endif;
+
+        if (config('laraflow.auth.allow_password_reset')):
+            Route::get('/forgot-password', [PasswordResetController::class, 'create'])
+                ->middleware('guest')
+                ->name('password.request');
+        endif;
+
+        Route::post('/forgot-password', [PasswordResetController::class, 'store'])
             ->middleware('guest')
-            ->name('password.request');
-    endif;
+            ->name('password.email');
 
-    Route::post('/forgot-password', [PasswordResetController::class, 'store'])
-        ->middleware('guest')
-        ->name('password.email');
+        Route::get('/reset-password/{token}', [PasswordResetController::class, 'edit'])
+            ->middleware('guest')
+            ->name('password.reset');
 
-    Route::get('/reset-password/{token}', [PasswordResetController::class, 'edit'])
-        ->middleware('guest')
-        ->name('password.reset');
+        Route::post('/reset-password', [PasswordResetController::class, 'update'])
+            ->middleware('guest')
+            ->name('password.update');
 
-    Route::post('/reset-password', [PasswordResetController::class, 'update'])
-        ->middleware('guest')
-        ->name('password.update');
+        Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+            ->middleware('auth')
+            ->name('verification.notice');
 
-    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
-        ->middleware('auth')
-        ->name('verification.notice');
+        Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+            ->middleware(['auth', 'signed', 'throttle:6,1'])
+            ->name('verification.verify');
 
-    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-        ->middleware(['auth', 'signed', 'throttle:6,1'])
-        ->name('verification.verify');
+        Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware(['auth', 'throttle:6,1'])
+            ->name('verification.send');
 
-    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware(['auth', 'throttle:6,1'])
-        ->name('verification.send');
+        Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+            ->middleware('auth')
+            ->name('password.confirm');
 
-    Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->middleware('auth')
-        ->name('password.confirm');
+        Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+            ->middleware('auth');
 
-    Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
-        ->middleware('auth');
-
-    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->middleware('auth')
-        ->name('logout');
-});
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->middleware('auth')
+            ->name('logout');
+    });
 
 /*Route::group(function () {
 
