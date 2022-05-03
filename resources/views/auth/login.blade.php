78cd2e9d66e64ab900809684eaa8a86b2cba9dd2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-05-03 14:30:38 +0600

    login email field added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,36 +19,36 @@
             <p class="login-box-msg">Sign in to start your session</p>
             {!! \Form::open(['route' => 'backend.auth.login', 'id' => 'login-form', 'method' => 'post']) !!}
 
-            @if(config('auth.credential_field') == \Constant::LOGIN_EMAIL
-            || (config('auth.credential_field') == \Constant::LOGIN_OTP
-                && config('auth.credential_otp_field') == \Constant::OTP_EMAIL))
+            @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_EMAIL
+            || (config('laraflow.auth.credential_field') == \Constant::LOGIN_OTP
+                && config('laraflow.auth.credential_otp_field') == \Constant::OTP_EMAIL))
                 {!! \Form::iEmail('email', __('Email'), null, true, "fas fa-envelope", "after",
                                     [ 'minlength' => '5', 'maxlength' => '250',
                                         'size' => '250', 'placeholder' => 'Enter Email Address']) !!}
             @endif
 
-            @if(config('auth.credential_field') == \Constant::LOGIN_MOBILE
-            || (config('auth.credential_field') == \Constant::LOGIN_OTP
-                && config('auth.credential_otp_field') == \Constant::OTP_MOBILE))
+            @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_MOBILE
+            || (config('laraflow.auth.credential_field') == \Constant::LOGIN_OTP
+                && config('laraflow.auth.credential_otp_field') == \Constant::OTP_MOBILE))
                 {!! \Form::iTel('mobile', __('Mobile'), null, true, "fas fa-mobile", "after",
                                     [ 'minlength' => '11', 'maxlength' => '11',
                                         'size' => '11', 'placeholder' => 'Enter Mobile Number']) !!}
             @endif
 
-            @if(config('auth.credential_field') == \Constant::LOGIN_USERNAME)
+            @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_USERNAME)
                 {!! \Form::iText('username', __('Username'), null, true, "fas fa-user-shield", "after",
                                     [ 'minlength' => '5', 'maxlength' => '250',
                                         'size' => '250', 'placeholder' => 'Enter Username']) !!}
             @endif
 
-            @if(config('auth.credential_field') != \Constant::LOGIN_OTP)
+            @if(config('laraflow.auth.credential_field') != \Constant::LOGIN_OTP)
                 {!! \Form::iPassword('password', __('Password'), true, "fas fa-lock", "after",
                                     ["placeholder" => 'Enter Password', 'autocomplete' => "current-password",
                                      'minlength' => '5', 'maxlength' => '250', 'size' => '250']) !!}
             @endif
 
             <div class="row">
-                @if(config('auth.allow_remembering'))
+                @if(config('laraflow.auth.allow_remembering'))
                     <div class="col-8">
                         <div class="icheck-primary">
                             {!! \Form::checkbox('remember', 'yes', null, ['id' => 'remember_me']) !!}
@@ -59,7 +59,7 @@
                     </div>
                     <!-- /.col -->
                 @endif
-                <div class="@if(!config('auth.allow_remembering')) offset-8 @endif col-4">
+                <div class="@if(!config('laraflow.auth.allow_remembering')) offset-8 @endif col-4">
                     <button type="submit" class="btn btn-primary btn-block">{{ __('Log in') }}</button>
                 </div>
                 <!-- /.col -->
@@ -79,15 +79,15 @@
             --}}
         <!-- /.social-auth-links -->
 
-            @if (Route::has('backend.auth.password.request'))
+            @if (\Route::has('backend.auth.password.request'))
                 <p class="mb-1">
                     <a href="{{ route('backend.auth.password.request') }}">I forgot my password</a>
                 </p>
             @endif
 
-            @if(Route::has('backend.auth.register'))
+            @if(\Route::has('backend.auth.register'))
                 <p class="mb-0">
-                    <a href="{{ route('backend.auth.register') }}" class="text-center">Register a new membership</a>
+                    <a href="{{ route('backend.auth.register') }}" class="text-center">Register a new account</a>
                 </p>
             @endif
         </div>
@@ -106,9 +106,9 @@
         $(function () {
             $("#login-form").validate({
                 rules: {
-                    @if(config('auth.credential_field') == \Constant::LOGIN_EMAIL
-                    || (config('auth.credential_field') == \Constant::LOGIN_OTP
-                    && config('auth.credential_otp_field') == \Constant::OTP_EMAIL))
+                    @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_EMAIL
+                    || (config('laraflow.auth.credential_field') == \Constant::LOGIN_OTP
+                    && config('laraflow.auth.credential_otp_field') == \Constant::OTP_EMAIL))
                     email: {
                         required: true,
                         minlength: 3,
@@ -117,9 +117,9 @@
                     },
                     @endif
 
-                        @if(config('auth.credential_field') == \Constant::LOGIN_MOBILE
-                        || (config('auth.credential_field') == \Constant::LOGIN_OTP
-                        && config('auth.credential_otp_field') == \Constant::OTP_MOBILE))
+                        @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_MOBILE
+                        || (config('laraflow.auth.credential_field') == \Constant::LOGIN_OTP
+                        && config('laraflow.auth.credential_otp_field') == \Constant::OTP_MOBILE))
                     mobile: {
                         required: true,
                         minlength: 11,
@@ -128,7 +128,7 @@
                     },
                     @endif
 
-                        @if(config('auth.credential_field') == \Constant::LOGIN_USERNAME)
+                        @if(config('laraflow.auth.credential_field') == \Constant::LOGIN_USERNAME)
                     username: {
                         required: true,
                         minlength: 5,
@@ -136,10 +136,10 @@
                     },
                     @endif
 
-                        @if(config('auth.credential_field') != \Constant::LOGIN_OTP)
+                        @if(config('laraflow.auth.credential_field') != \Constant::LOGIN_OTP)
                     password: {
                         required: true,
-                        minlength: {{ config('auth.minimum_password_length') }},
+                        minlength: {{ config('laraflow.auth.minimum_password_length') }},
                         maxlength: 250
                     }
                     @endif
