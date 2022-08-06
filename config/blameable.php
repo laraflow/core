6772c5b6c556880a035de2e2824cd71ada9e4e9c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-06 12:05:13 +0600

    balme trait added

---
+++ config/blameable.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'columns' => [
+        'createdByAttribute' => 'created_by',
+        'updatedByAttribute' => 'updated_by',
+        'deletedByAttribute' => 'deleted_by',
+    ],
+    'models' => [
+        'user' => \App\Models\Backend\Setting\User::class,
+    ],
+    'foreign_id' => 'id',
+];
